acba91682345d9816da35d5dbebd0fea6a282140
silver0511 <shiyunjie0511@163.com>
2018-06-22 15:36:13 +0800

    Update build_clean.sh

--- build_clean.sh
+++ build_clean.sh
@@ -2,9 +2,15 @@
 echo "clean build cache start...................."
 rm -rf build/debug/*
 rm -rf build/release/*
+rm -rf build
 echo "clean build cache success.................."
 
 echo "clean bin cache start......................"
 rm -rf bin/debug/*
 rm -rf bin/release/*
+rm -rf bin
 echo "clean bin cache success...................."
+
+echo "clean lib cache start......................"
+rm -rf lib
+echo "clean lib cache end........................"
