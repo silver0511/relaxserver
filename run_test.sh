11415ee300a01e8e63994a7fe9caea8c70e12bb9
silver0511 <shiyunjie0511@163.com>
2018-06-22 16:27:00 +0800

    create run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+COMPILE_TYPE=$1
+ROOT_DIR=`pwd`
+if [ -z ${COMPILE_TYPE} ];
+then
+    echo -e "error need a param  --> usage ./build.sh  [debug] or [release]-----------------------"
+    exit
+fi
+
+function readfile ()
+{
+  for file in `ls $1`
+  do
+    if [ -d $1"/"$file ]
+    then
+      readfile $1"/"$file
+    elif [ -x $1"/"$file ]
+    then
+      $1"/"$file
+    fi
+  done
+}
+
+TEST_ROOT_DIR=$ROOT_DIR/bin/$COMPILE_TYPE/test
+if [ ! -d $TEST_ROOT_DIR ];
+then
+    echo -e "error test directory  $TEST_ROOT_DIR is not exsist------------"
+    exit
+fi
+
+
+readfile $TEST_ROOT_DIR
